8fdcfc769a9813395c0387c209f89295fb9a2baf
NAI-21-12 <ianvaleriost@gmail.com>
2025-06-12 21:09:56 -0600

    Repetir_infinito progra03.py

--- progra03.py
+++ progra03.py
@@ -1,1 +1,15 @@
 print("Hola Eduardo")
+
+import time as t
+def repetir_infinito(palabra):
+    """
+    Esto repite una palabra indefinidamente
+    E: Un texto
+    S: Un texto infinito
+    R: Texto tipo str
+    """
+    while len(palabra) > 0:
+        print(palabra)
+        t.sleep(0.5)
+
+repetir_infinito("NWord")
