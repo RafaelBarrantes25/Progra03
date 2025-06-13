9536eff5f5eeddf57573ba86fcc24490ae1395d8
RafaelBarrantes25 <jorabaqu2580@gmail.com>
2025-06-12 21:13:15 -0600

    no

--- progra03.py
+++ progra03.py
@@ -1,15 +1,3 @@
 print("Hola Eduardo")
 
-import time as t
-def repetir_infinito(palabra):
-    """
-    Esto repite una palabra indefinidamente
-    E: Un texto
-    S: Un texto infinito
-    R: Texto tipo str
-    """
-    while len(palabra) > 0:
-        print(palabra)
-        t.sleep(0.5)
-
-repetir_infinito("NWord")
+#Sea sorompiado eso no se dice
