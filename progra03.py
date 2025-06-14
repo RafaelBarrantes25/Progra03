c16f166f386cc55ef80d527dbb25f100ffa870bc
RafaelBarrantes25 <jorabaqu2580@gmail.com>
2025-06-13 20:32:49 -0600

    prueba para elegir cartas

--- progra03.py
+++ progra03.py
@@ -1,3 +1,13 @@
-print("Hola Eduardo")
+import random
+import time
 
-#Sea sorompiado eso no se dice
+def escoger_carta():
+    palos = ["picas","corazones","diamantes","tréboles"]
+    números = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
+    elección_palos = random.randint(0,3)
+    elección_números = random.randint(0,12)
+
+    print(f"Salió el {números[elección_números]} de {palos[elección_palos]}")
+
+escoger_carta()
