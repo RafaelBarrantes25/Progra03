d481ca9d6d01abd131a2a03d107c38bdb670aacd
RafaelBarrantes25 <jorabaqu2580@gmail.com>
2025-06-15 16:40:17 -0600

    sirve que la cpu tome el as como 1 o 11 según convenga

--- progra03.py
+++ progra03.py
@@ -3,30 +3,67 @@ import time
 
 def escoger_carta():
     palos = ["picas","corazones","diamantes","tréboles"]
-    números = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    números = [1,2,3,4,5,6,7,8,9,10,11]
 
-    elección_palos = random.randint(0,3)
-    elección_números = random.randint(0,12)
+    elección_palos = palos[random.randint(0,3)]
+    elección_números = números[random.randint(0,10)]
 
-    return elección_números
+    return elección_números,elección_palos
 
 
-def fascistas_cartas():
-
+def fascistas_cartas(lista):
+    
     valor = 0
-    turno = True
-    while turno:
-        time.sleep(2)
-        if valor == 21:
-            print("Consiguió 21")
-            turno = False
-        elif valor < 21:
-            print("Agarra carta")
-            valor += escoger_carta()
-            print(valor)
-        else:
-            print("El valor se pasó de 21")
-            turno = False
-    print("Hola eduardo")
+    valor_obtenido, palo_obtenido = escoger_carta()
 
-fascistas_cartas()
+
+    for número in range(len(lista)):
+        valor += lista[número]
+    
+    if valor == 21:
+        print("Tiene 21")
+    else:
+        if valor < 21:
+            if valor_obtenido == 1:
+                lista += [11]
+                print(lista)
+            else:
+                lista += [valor_obtenido]
+                print(lista)
+        else:
+
+            for número in range(len(lista)):
+                if lista[número] == 11:
+                    lista[número] = 1
+                    print(lista)
+              
+    
+
+
+    print(lista)
+    return lista
+            
+    
+    
+
+    
+
+def juego(lista_fascistas):
+    
+    if lista_fascistas == []:
+        lista = []
+    else:
+        lista = lista_fascistas
+    lista_fascistas = fascistas_cartas(lista)
+
+    fascistas_cartas(lista_fascistas)
+    fascistas_cartas(lista_fascistas)
+    fascistas_cartas(lista_fascistas)
+    fascistas_cartas(lista_fascistas)
+    fascistas_cartas(lista_fascistas)
+    fascistas_cartas(lista_fascistas)
+    fascistas_cartas(lista_fascistas)
+    fascistas_cartas(lista_fascistas)
+    fascistas_cartas(lista_fascistas)
+    fascistas_cartas(lista_fascistas)
+juego([])
