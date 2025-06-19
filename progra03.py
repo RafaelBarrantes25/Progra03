ca6ffdee513cf782cbd7056f888299f4ad9962cb
NAI-21-12 <ianvaleriost@gmail.com>
2025-06-18 18:57:43 -0600

    Un poco de PEP 8 y documentación chafa

--- progra03.py
+++ progra03.py
@@ -1,7 +1,18 @@
+##Imports
+
 import random
 import time
 
+
+##Funciones
+
 def escoger_carta():
+    """
+    Esto ¿escoge una carta?
+    E:
+    S:
+    R:
+    """
     palos = ["picas","corazones","diamantes","tréboles"]
     números = [1,2,3,4,5,6,7,8,9,10,11]
 
@@ -12,48 +23,59 @@ def escoger_carta():
 
 
 def fascistas_cartas(lista):
-    
+    """
+    Si, hace algo
+    E: 
+    S: 
+    R: 
+    """
     valor = 0
     valor_obtenido, palo_obtenido = escoger_carta()
 
-
-    for número in range(len(lista)):
+    #for número in range(len(lista)): (Versión Rafael)
+    for número in lista:
+    #Versión Ian (Creo que gasta menos recursos, no me haga caso, ni siquiera sé si funciona)
         valor += lista[número]
-    
+
     if valor == 21:
         print("Tiene 21")
+
     else:
         if valor < 21:
             if valor_obtenido == 1:
                 lista += [11]
                 print(lista)
+
             else:
                 lista += [valor_obtenido]
                 print(lista)
+
         else:
 
             for número in range(len(lista)):
                 if lista[número] == 11:
                     lista[número] = 1
                     print(lista)
-              
-    
-
 
     print(lista)
     return lista
-            
-    
-    
 
-    
+
+##Juego
 
 def juego(lista_fascistas):
-    
+    """
+    Esto corre el juego
+    E: Una lista
+    S: La ejecución del juego
+    R: La lista debe ser vacía
+    """
     if lista_fascistas == []:
         lista = []
+
     else:
         lista = lista_fascistas
+
     lista_fascistas = fascistas_cartas(lista)
 
     fascistas_cartas(lista_fascistas)
@@ -66,4 +88,5 @@ def juego(lista_fascistas):
     fascistas_cartas(lista_fascistas)
     fascistas_cartas(lista_fascistas)
     fascistas_cartas(lista_fascistas)
+
 juego([])
