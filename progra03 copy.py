e170814d2c3a502cc0f07b6bf3e7ed81de138ca5
RafaelBarrantes25 <jorabaqu2580@gmail.com>
2025-06-20 21:20:41 -0600

    SIRVE SIRVE se asignan las cartas iniciales y hay una función que cuenta cuánto tiene y ya

--- progra03 copy.py
+++ progra03 copy.py
@@ -3,7 +3,7 @@ import time
 
 def escoger_carta():
     palos = ["picas","corazones","diamantes","tréboles"]
-    números = [1,2,3,4,5,6,7,8,9,10,11]
+    números = [1,2,3,4,5,6,7,8,9,"J","Q","K",11]
     
     elección_palos = palos[random.randint(0,3)]
     elección_números = números[random.randint(0,10)]
@@ -16,56 +16,37 @@ def asignar_primeras(lista,lista_p):
     valor = 0
     valor_obtenido, palo_obtenido = escoger_carta()
 
-
-    for número in range(len(lista)):
-        valor += lista[número]
-    
-    if valor == 21:
-        return lista,lista_p
-    
-    elif valor == 19 or valor == 20:
-        elección = random.randint(1,2)
-        if elección == 1:
+ 
+    if valor < 21:
+        if valor_obtenido == 1:
+            lista += [11]
+            lista_p += [palo_obtenido]
+        else:
             lista += [valor_obtenido]
             lista_p += [palo_obtenido]
-        
     else:
-        if valor < 21:
-            if valor_obtenido == 1:
-                lista += [11]
-                lista_p += [palo_obtenido]
-            else:
-                lista += [valor_obtenido]
-                lista_p += [palo_obtenido]
-        else:
 
-            for número in range(len(lista)):
-                if lista[número] == 11:
-                    lista[número] = 1
+        for número in range(len(lista)):
+            if lista[número] == 11:
+                lista[número] = 1
                     
     return lista,lista_p
-            
-def revisar_especiales(lista):
 
-    for carta in range(len(lista)-1):
-        if lista[carta] == 10:
-            elección = random.randint(1,3)
-            if elección == 1:
-                lista[carta] = ["J"]
-            if elección == 2:
-                lista[carta] = ["Q"]
-            if elección == 3:
-                lista[carta] = ["K"]
-
-    return lista
+def contar(lista):
+    contador = 0
+    for valor in range(len(lista)):
+        if type(lista[valor]) == str:
+            contador += 10
+        else:
+            contador += lista[valor]
+    return contador
 
 def juego(lista_f=[],lista_p_f=[],lista_j=[],lista_p_j=[]):
     if lista_j == []:
         for carta in range(2):
             lista_j, lista_p_j = asignar_primeras(lista_j, lista_p_j)
             lista_f, lista_p_f = asignar_primeras(lista_f, lista_p_f)
-    lista_j = revisar_especiales(lista_j)
-    lista_f = revisar_especiales(lista_f)
+
     if lista_f[1] == 11 or lista_f[1] == 1:
         print(
             f"La segunda carta de los fascistas es un as de {lista_p_f[1]}")
@@ -79,8 +60,9 @@ def juego(lista_f=[],lista_p_f=[],lista_j=[],lista_p_j=[]):
     else:   
         print(
             f"La segunda carta del jugador es un {lista_j[1]} de {lista_p_j[1]}")
-
-    
+    print(lista_j)
+    print(lista_p_j)
+    print(contar(lista_j))
 
 
 
